c42514914a90a4f6e2674b7f1523c3a76e13f1ba
batkor <batkor@3422153.no-reply.drupal.org>
2021-06-20 15:23:14 +0000

    update photoswipe_requirements, correct code standard photoswipe.api.php

--- photoswipe.api.php
+++ photoswipe.api.php
@@ -23,20 +23,17 @@ function hook_photoswipe_js_options_alter(array &$settings) {
       'url' => 'https://www.facebook.com/sharer/sharer.php?u={{url}}',
     ],
     [
-      'id' =>
-      'twitter',
+      'id' => 'twitter',
       'label' => t('Tweet'),
       'url' => 'https://twitter.com/intent/tweet?text={{text}}&url={{url}}',
     ],
     [
-      'id' =>
-      'pinterest',
+      'id' => 'pinterest',
       'label' => t('Pin it'),
       'url' => 'http://www.pinterest.com/pin/create/button/?url={{url}}&media={{image_url}}&description={{text}}',
     ],
     [
-      'id' =>
-      'download',
+      'id' => 'download',
       'label' => t('Download image'),
       'url' => '{{raw_image_url}}',
       'download' => TRUE,
